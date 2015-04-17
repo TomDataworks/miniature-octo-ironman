c0605fb1b2d039fc96323518f3f1b11d20d27a7f
Arven <brian.johnathan.b@gmail.com>
2015-04-16 20:34:12 -0400

    Fix up wake packet stuffs

--- makepacket.sh
+++ makepacket.sh
@@ -1,3 +1,6 @@
 #!/bin/dash
 ETHER="00:25:22:60:D4:72"
-echo ${ETHER} | xxd -r -p > wake.packet
+ETHER2=`echo $ETHER | sed "s/://g"`
+ETHER3="${ETHER2}${ETHER2}${ETHER2}${ETHER2}"
+ETHER4="FFFFFFFFFFFF${ETHER3}${ETHER3}${ETHER3}${ETHER3}"
+echo ${ETHER4} | xxd -r -p > wake.packet
--- manager.sh
+++ manager.sh
@@ -6,6 +6,7 @@ while [ 1 ]; do
   if [ -f "$@" ]; then
     echo "Remote host has gone down @ $(date)"
     rm "$WAKEFILE"
+    
   fi
 done
 
